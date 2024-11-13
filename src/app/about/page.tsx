c1475c8895e523bc954b1e0440e7f7d2b257a55e
maryasheikh <leslierose645@gmail.com>
2024-11-13 17:13:20 +0500

    first commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,10 @@
 export default function AboutPage() {
     
     return(
-        <div className="flex justify-center text-center items-center text-blue-700 h-screen bg-slate-200 text-2xl font-serif "  >
+        <div className= "flex justify-center text-center items-center text-blue-700 h-screen bg-slate-200 text-2xl font-serif"  > 
 
             <h1>About Page</h1>
 
         </div>
-    )
+    );
 }
--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,9 +1,15 @@
 export default function JobPage() {
+  throw new Error ('Job page not yet implemented')
   
   
     return(
-      <div className="flex justify-center text-center items-center text-red-700 h-screen bg-red-200 text-2xl font-serif"  >
-        <h1>Jobs Page</h1>
+      <div >
       </div>
     );
-}
+  }
+
+
+
+
+
+  
